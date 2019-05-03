94145e97baf07d6ef3184ad30baeeb925887e9c7
Rebecca Strauss <rstrauss127@gmail.com>
2019-05-03 21:42:21 +0000

    Automatically backed up by Learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -4,6 +4,6 @@ module SongsHelper
       select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
     else
       hidden_field_tag "song[artist_id]", song.artist_id
-    end 
+    end
   end
 end
